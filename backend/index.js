56eb43fd48da3230f3cc4cd754734f51457349af
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-10-08 23:35:09 +0530

    initial setup

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const express = require("express");
+const server = express();
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log(`Server Started at PORT ${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="bg-slate-600 text-4xl">App</div>;
+};
+
+export default App;
