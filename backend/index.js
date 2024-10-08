5556760f9dc3a8a424bca43803a17f40289a2fbb
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-10-09 03:35:53 +0530

    chore: configure CORS

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-
+const cors = require("cors");
 const { dbConnect } = require("./dbConnect");
 const router = require("./routes/userRoute");
 const server = express();
 dbConnect();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server Started at PORT ${PORT}`));
