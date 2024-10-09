80a55dd0b2c723f3418ad84b98e597e3fb37a4f3
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-10-09 23:21:54 +0530

    feat(server): add JSON and URL-encoded body parsers

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,17 @@ const router = require("./routes/userRoute");
 const server = express();
 dbConnect();
 
-app.use(
+server.use(express.json());
+server.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+server.use(express.urlencoded({ extended: false })); //handle form data
+
+server.use("/api", router);
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server Started at PORT ${PORT}`));
