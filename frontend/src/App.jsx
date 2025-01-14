7af0661c281a4fce0d8a16520945e516d4b7f25d
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2025-01-14 23:59:47 +0530

    feat: use Outlet instead of Body for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Auction from "./components/Auction";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
