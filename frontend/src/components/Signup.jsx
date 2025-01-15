87bed9cdf90e79d3c210bacfca190bbf0b0c2f24
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2025-01-15 23:48:25 +0530

    feat: add signup form with API and toast

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,34 +1,50 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { FaUserPlus } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Signup = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const Navigate = useNavigate();
   console.log(fullName, email, password);
 
   const handleSignup = (e) => {
     e.preventDefault();
     axios
       .post(
-        "http://localhost:8080/api/signup",
-        {
-          fullName,
-          email,
-          password,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
+        "http://localhost:8080/api/user/signup",
+        { fullName, email, password },
+        { headers: { "Content-Type": "application/json" } }
       )
-      .then((result) => console.log(result))
-      .catch((err) => console.log(err));
+      .then((result) => {
+        toast.success("Signup successful. Please Signin to your new account.");
+        console.log(result);
+        Navigate("/signin");
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.response && err.response.data) {
+          toast.error(`Signup failed: ${err.response.data.error}`);
+        } else {
+          toast.error("Signup failed. Please try again.");
+        }
+      });
   };
+
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={true}
+        closeOnClick
+        pauseOnFocusLoss={false}
+      />
       <form
         onSubmit={handleSignup}
         className="w-full max-w-sm bg-white p-6 rounded-lg shadow-lg"
@@ -89,6 +105,16 @@ const Signup = () => {
           <FaUserPlus className="mr-2" />
           Sign Up
         </button>
+
+        <p className="mt-4 text-center text-sm text-gray-600">
+          Already have an account?{" "}
+          <Link
+            to="/signin"
+            className="text-blue-500 hover:text-blue-700 font-medium"
+          >
+            Sign In
+          </Link>
+        </p>
       </form>
     </div>
   );
